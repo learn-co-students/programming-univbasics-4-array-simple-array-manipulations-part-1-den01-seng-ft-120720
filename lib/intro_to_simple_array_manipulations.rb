e6f84a5561c3ab5c6ad01f4eef44e6edfcf1908b
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-18 02:14:02 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_push(some_array, some_string)
+some_array.push some_string
+end
+def using_unshift(some_array, some_string)
+    some_array.unshift some_string
+  end
+  def using_pop(continents)
+    continents.pop
+  end
+  def pop_with_args(dog_breeds)
+    dog_breeds.pop(2)
+  end
+  def using_shift(my_favorite_cities)
+    my_favorite_cities.shift
+  end
+  def shift_with_args(ice_cream_brands)
+    ice_cream_brands.shift(2)
+  end
